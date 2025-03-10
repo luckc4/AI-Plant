c8f64cabaf21578d44cbe1c3a7b0bc9bcfc55b13
luckc4 <1143829504@qq.com>
2025-03-10 15:03:47 +0800

    添加AI对话功能

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 App({
   onLaunch: function() {
     // Initialize storage for logs if not exists
+
+    if (!wx.cloud) {
+      console.error("请使用 2.2.3 或以上的基础库以使用云能力");
+    } else {
+      wx.cloud.init({
+        env: "cloud1-4gaqbk1a32ca7043",
+        traceUser: true,
+      });
+    }
+
     wx.getStorage({
       key: 'logs',
       fail: function() {
--- pages/analysis/analysis.js
+++ pages/analysis/analysis.js
@@ -1,66 +1,61 @@
-// pages/analysis/analysis.js
+// pages/chatBot/chatBot.js
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-
+    agentConfig: {
+      type: "bot", // 值为'bot'或'model'。当type='bot'时，botId必填；当type='model'时，modelName和model必填
+      botId: "bot-2ce669c4", // agent id
+      
+    },
   },
 
+  // modelName: "hunyuan-open", // 大模型服务商
+  // model: "hunyuan-lite", // 具体的模型版本
+
+  // modelName: "deepseek", // 大模型服务商
+  // model: "deepseek-r1", // 具体的模型版本
+
+  // modelName: "deepseek", // 大模型服务商
+  // model: "deepseek-v3", // 具体的模型版本
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad(options) {
-
-  },
+  onLoad(options) {},
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady() {
-
-  },
+  onReady() {},
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow() {
-
-  },
+  onShow() {},
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide() {
-
-  },
+  onHide() {},
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload() {
-
-  },
+  onUnload() {},
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh() {
-
-  },
+  onPullDownRefresh() {},
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom() {
-
-  },
+  onReachBottom() {},
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage() {
-
-  }
-})
+  onShareAppMessage() {},
+});
